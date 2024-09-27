094a1e7eddbc240fb926fcbc5235422b56445141
Smogg21 <spazzaglia98@gmail.com>
2024-09-26 23:39:29 -0500

    Worked on app

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -2,11 +2,16 @@ import '../gesture-handler';
 import React from 'react';
 import {StackNavigator} from './presentation/navigator/StackNavigator';
 import {ThemeContextProvider} from './presentation/context/ThemeContext';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const PokedexApp = () => {
   return (
-    <ThemeContextProvider>
-      <StackNavigator />
-    </ThemeContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <StackNavigator />
+      </ThemeContextProvider>
+    </QueryClientProvider>
   );
 };
